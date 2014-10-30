a3e796b899734715fd68d7cf2ef46367b80a2f73
Skvortsoff <tirey_sss@ukr.net>
2014-10-30 12:45:31 +0200

    Update tests Junit, corrected employee.jsp

--- src/test/java/com/academysmart/repositorty/EmployeeRepositorySingletonTest.java
+++ src/test/java/com/academysmart/repositorty/EmployeeRepositorySingletonTest.java
@@ -1,7 +1,9 @@
 package com.academysmart.repositorty;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.academysmart.exception.ServletException;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -10,22 +12,28 @@ import com.academysmart.exception.IncorrectEmailException;
 import com.academysmart.repository.EmployeeRepositorySingleton;
 
 public class EmployeeRepositorySingletonTest {
-
+ private static EmployeeRepositorySingleton ers1;
+ private static EmployeeRepositorySingleton ers2;
 	@BeforeClass
 	public static void beforeClass()throws Exception {
-//		EmployeeRepositorySingleton.getRepository().addEmployee("Иван",
-//				"Иванов", "ivanov@mail.ru");
+		EmployeeRepositorySingleton.getRepository().addEmployee("Vasya",
+			"Ivanov", "ivanov@mail.ru");
 	}
 
 	@Test
-	public void testGetRepositoryReturnOneInstance() {
-		Assert.fail("Test is not implemented");
+	public void testGetRepositoryReturnOneInstance() throws ServletException {
+        ers1=EmployeeRepositorySingleton.getRepository();
+        ers1.addEmployee("Vasya", "Ivanov", "ivanov@gmail.com");
+        ers2=EmployeeRepositorySingleton.getRepository();
+		    assertEquals("Test is not implemented", ers2, ers1);
 	}
 	
 	
-	@Test(expected=IncorrectEmailException.class)
-	public void testAddEmployeWithIncorrectEmail() throws IncorrectEmailException  {
-		Assert.fail("Test is not implemented");
+	@Test
+            (expected=IncorrectEmailException.class)
+	public void testAddEmployeeWithIncorrectEmail() throws ServletException  {
+        EmployeeRepositorySingleton.getRepository().addEmployee("Ilya","Ivanov", "ivanov@mail.ru");
+        Assert.fail("Test is not implemented");
 	}
 
 }
